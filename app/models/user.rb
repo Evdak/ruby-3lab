58a604e83ff5a73e9feb84a899e94d5c80e95342
Evdak <breed.kir.evd@ya.ru>
2022-11-16 10:19:02 +0300

    finish 8 lab

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,21 +7,23 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :email, presence: true, length: { maximum: 255 },
-          format: { with: VALID_EMAIL_REGEX },
-          uniqueness: { case_sensitive: false }
+      format: { with: VALID_EMAIL_REGEX },
+      uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
+  class << self
   # Возвращает дайджест данной строки
-  def User.digest(string)
+  def digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                          BCrypt::Engine.cost
+              BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
   
   # Возвращает случайный токен
-  def User.new_token
+  def new_token
     SecureRandom.urlsafe_base64
+  end
   end
 
   # Запоминает пользователя в базе данных для использования в постоянной сессии.
